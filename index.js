d70986898c14f489bcdb3c4fab58ebce54222ebb
Karen <devkarenc@gmail.com>
2021-01-14 00:40:30 -0500

    Basic console version of RPS game

---
+++ index.js
@@ -0,0 +1,58 @@
+// Basic console version of Rock, Paper, Scissors Game
+
+// randomly return either Rock, Paper, or Scissors
+function computerPlay() {
+  const choices = ["Rock", "Paper", "Scissors"];
+  const randomChoice = choices[Math.floor(Math.random() * choices.length)];
+  return randomChoice;
+}
+
+// game of five rounds
+function game() {
+  // one round of RPS game
+  function playRound(playerSelection, computerSelection) {
+    const choices = ["Rock", "Paper", "Scissors"];
+    const capPlayerSelection = `${playerSelection[0].toUpperCase()}${playerSelection
+      .substr(1)
+      .toLowerCase()}`;
+    if (choices.includes(capPlayerSelection)) {
+      if (capPlayerSelection === computerSelection) {
+        return `It's a Tie! You chose ${capPlayerSelection} and the computer also chose ${computerSelection}.`;
+      } else if (
+        (capPlayerSelection === "Rock" && computerSelection === "Paper") ||
+        (capPlayerSelection === "Paper" && computerSelection === "Scissors") ||
+        (capPlayerSelection === "Scissors" && computerSelection === "Rock")
+      ) {
+        computerScore++;
+        return `You Lose. You chose ${capPlayerSelection} and the computer chose ${computerSelection}.`;
+      } else if (
+        (capPlayerSelection === "Rock" && computerSelection === "Scissors") ||
+        (capPlayerSelection === "Paper" && computerSelection === "Rock") ||
+        (capPlayerSelection === "Scissors" && computerSelection === "Paper")
+      ) {
+        playerScore++;
+        return `You Win! You chose ${capPlayerSelection} and the computer chose ${computerSelection}.`;
+      }
+    } else {
+      return `Please choose either Rock, Paper, or Scissors.`;
+    }
+  }
+  let playerScore = 0;
+  let computerScore = 0;
+  for (let i = 0; i < 5; i++) {
+    const playerSelection = window.prompt(
+      "Are you ready to play? Rock, Paper, or Scissors?"
+    );
+    const computerSelection = computerPlay();
+    console.log(playRound(playerSelection, computerSelection));
+  }
+  if (playerScore > computerScore) {
+    return `You win!`;
+  } else if (playerScore < computerScore) {
+    return `You lose.`;
+  } else {
+    return `It's a tie.`;
+  }
+}
+
+console.log(game());
